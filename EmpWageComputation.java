f6f88b2a7b862fd54f29dbc04b4577e124b7ef79
Mithu8521 <mithileshkr8521@gmail.com>
2021-08-21 18:10:29 +0530

    Add Part time

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,12 +1,17 @@
 public class EmpWageComputation {
     public static void main(String[] args) {
 	int Full_Time = 1;
+	int rate_per_hour = 20;
+        int emp_wage = 0;
+        int emp_hrs = 0;
         double empCheck = Math.floor(Math.random() *10 %2);
         if(empCheck==Full_Time) {
-            System.out.println("Employee is present");
+            emp_hrs=8;
 	}
         else {
-            System.out.println("Employee is Absent");
+	    emp_hrs=0;
+	    emp_wage=rate_per_hour*emp_hrs;
+            System.out.println("Total Wage Of Employee" +emp_wage);
 	}
     }
 }
